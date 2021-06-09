ca23ffebd206945509ae168603a997a229cb5ac2
Flavio <flavio.origgi@outlook.it>
2021-06-09 10:45:01 +0200

    fatto bonus

--- js/script.js
+++ js/script.js
@@ -15,15 +15,40 @@
 // -Al termine della partita il software deve comunicare il punteggio, 
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
 
+// BONUS: (da fare solo se funziona tutto il resto) all’inizio il software 
+// richiede anche una difficoltà all’utente che cambia il range di numeri casuali: 
+// - difficoltà 0 => tra 1 e 100
+// - difficoltà 1 => tra 1 e 80
+// - difficoltà 2 => tra 1 e 50
+
 
 var start = document.getElementById('play');
 start.addEventListener("click", function(){
+    
+    
+    / --- sezione bonus --- /
+    var numMax = 100;
+    var scelta = false;
+    // richiedo all’utente una difficoltà che cambia il range di numeri casuali
+    while (!scelta){
+        scelta = prompt('scegli un livello da 0 a 2, dove Zero sarà il più semplice e Due il piu difficile');        
+        if (scelta == 0){
+            numMax = 100;            
+        } else if(scelta == 1) {
+            numMax = 80;            
+        } else if(scelta == 2) {  
+            numMax = 50;            
+        } else{
+            alert('per scegliere il livello, il numero inserito deve essere tra 0 e 2.');
+            return false;
+        }
+    }
 
     / --- sezione pc --- /
     // generatore numeri univoci random del pc
     var listaNumeriPc =[];
     while(listaNumeriPc.length < 16){
-        var numeroRandomPc = Math.floor(Math.random() * 99) + 1;
+        var numeroRandomPc = Math.floor(Math.random() * numMax) + 1;
         if(listaNumeriPc.indexOf(numeroRandomPc) === -1) listaNumeriPc.push(numeroRandomPc);
     }
     console.log(listaNumeriPc);
